ab8543e2e774a5a46f1c1cb62c60ff535e922e2c
vajahat-kamal-new <vajahatkamal@gmail.com>
2025-04-18 14:17:31 +0500

    Update script.js

--- script.js
+++ script.js
@@ -158,5 +158,38 @@ window.addEventListener("load", () => {
     document.getElementById("darkModeToggle").checked = true;
   }
 });
+function generatePassword() {
+  const length = document.getElementById("length").value;
+  const useUpper = document.getElementById("uppercase").checked;
+  const useLower = document.getElementById("lowercase").checked;
+  const useNumbers = document.getElementById("numbers").checked;
+  const useSymbols = document.getElementById("symbols").checked;
+
+  let chars = "";
+  if (useUpper) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (useLower) chars += "abcdefghijklmnopqrstuvwxyz";
+  if (useNumbers) chars += "0123456789";
+  if (useSymbols) chars += "!@#$%^&*()_+[]{}|;:,.<>?";
+
+  if (chars === "") {
+    alert("Please select at least one character type!");
+    return;
+  }
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  document.getElementById("result").value = password;
+}
+
+function copyPassword() {
+  const password = document.getElementById("result");
+  password.select();
+  document.execCommand("copy");
+  alert("Password copied to clipboard!");
+}
+
 
 
